ef1b7ef3c5e27853a362d9f02819b3410f48a848
Ramón Alberto Gómez Urquídez <gomez.ramon2004@gmail.com>
2023-11-08 11:51:02 -0700

    modified DFS and BFS arguments

--- graph/appGrafo.hpp
+++ graph/appGrafo.hpp
@@ -6,7 +6,7 @@ class Graph {
     public:
         void loadGraph(int n, int m, std::vector<std::vector<bool>>& adj_matrix, std::list<int>* &adj_list);
         void addEdge(int u, int v, std::vector<std::vector<bool>>& adj_matrix, std::list<int>* &adj_list);
-        void DFS(int start, bool** adj_matrix);
-        void BFS(int start, std::list<int>* adj_list);
+        void DFS(int start, std::vector<std::vector<bool>>& adj_matrix);
+        void BFS(int start,  std::list<int>* &adj_list);
 };
 
